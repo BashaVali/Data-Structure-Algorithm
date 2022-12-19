9a37852052d6429f0cadaa66035b73fb45477044
BashaVali <bashavali222@gmail.com>
2022-12-19 09:27:01 +0530

    Bubble Sort

--- DataStructureAlgorithm/Program.cs
+++ DataStructureAlgorithm/Program.cs
@@ -12,8 +12,8 @@ class Program
 
             Console.WriteLine(" Select the option ");
 
-            Console.WriteLine("1.BinarySearch \n2.InsertionSort \n3.Exit ");
-                             
+            Console.WriteLine("1.BinarySearch \n2.InsertionSort \n3.BubbleSort \n4.Exit ");
+
             int choice = Convert.ToInt32(Console.ReadLine());
             switch (choice)
             {
@@ -27,8 +27,13 @@ class Program
                     InsertionSort insertion = new InsertionSort();
                     insertion.Sort(arr);
                     break;
-
                 case 3:
+                    BubbleSort bubble = new BubbleSort();
+                    int[] arr2 = { 5, 2, 1, 4, 3 };
+                    bubble.Sort(arr2);
+                    break;
+
+                case 4:
                     flag = false;
                     break;
             }
